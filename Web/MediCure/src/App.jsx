b079ac9c1f5270a9056887b2df0130143d5d4512
Sakshi <sakshi300699.b@gmail.com>
2022-03-05 23:13:03 +0530

    Login page added

--- Web/MediCure/src/App.jsx
+++ Web/MediCure/src/App.jsx
@@ -11,6 +11,7 @@ import { Contact } from "./components/contact";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
+import Login from './components/login';
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
@@ -28,12 +29,8 @@ const App = () => {
       <Navigation />
       <Header data={landingPageData.Header} />
       <Features data={landingPageData.Features} />
-      {/* <About data={landingPageData.About} /> */}
-      {/* <Services data={landingPageData.Services} />
-      <Gallery data={landingPageData.Gallery}/>
-      <Testimonials data={landingPageData.Testimonials} /> */}
       <Team data={landingPageData.Team} />
-      {/* <Contact data={landingPageData.Contact} /> */}
+      {/* <Login /> */}
     </div>
   );
 };
