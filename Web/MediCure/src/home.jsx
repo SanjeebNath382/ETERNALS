ced9d048d4480499d101a99fc443cd11797c322d
sanjeebnath382 <sanjeebnath382@gmail.com>
2022-03-06 00:23:59 +0530

    modified Home page images

--- Web/MediCure/src/home.jsx
+++ Web/MediCure/src/home.jsx
@@ -1,20 +1,8 @@
-import { useState, useEffect } from "react";
 import {
     BrowserRouter as Router,
     Routes,
     Route,
-    Link
   } from "react-router-dom";
-import { Navigation } from "./components/navigation";
-import { Header } from "./components/header";
-import { Features } from "./components/features";
-import { About } from "./components/about";
-import { Services } from "./components/services";
-import { Gallery } from "./components/gallery";
-import { Testimonials } from "./components/testimonials";
-import { Team } from "./components/Team";
-import { Contact } from "./components/contact";
-import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
 import "./App.css";
 import App from "./App";
